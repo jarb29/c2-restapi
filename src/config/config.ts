a70b4796e5553a9841f87c69ec9b0c5b1a83b468
jarb29 <jarb29@gmail.com>
2020-10-01 19:37:32 -0300

    .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,17 +3,17 @@ export const config = {
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.AWS_DATABASE,
-    "host": process.env.AWS_HOST
+    "host": process.env.AWS_HOST,
     "dialect": process.env.AWS_DIALECT,
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.udagram-alex-dev-udacity
+    "aws_media_bucket": process.env.AWS_BUCKET
   },
   "prod": {
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.AWS_DATABASE,
     "host": process.env.AWS_HOST,
-    "dialect": process.AWS_DIALECT
+    "dialect": process.env.AWS_DIALECT
   }
 }
